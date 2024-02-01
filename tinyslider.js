d1c0d8d4db0291f78c2d0de20bc37751394dcf2d
John Christian Z. Leocario <Jczleocario@Gmail.com>
2024-02-01 20:45:38 +0800

    first commit

---
+++ tinyslider.js
@@ -0,0 +1,13 @@
+let slider = tns({
+    container: ".my-slider",
+    speed: 400,
+    nav: false,
+    slideBy: 1,
+    controlsContainer: "#controls",
+    prevButton: ".previous",
+    nextButton: ".next",
+    loop:true,
+    items: 15,
+    viewportMax :true,
+    mouseDrag: true
+});
